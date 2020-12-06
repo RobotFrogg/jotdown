4d67bdd4baa0090aeb62ec783665726a6dc104b0
George Zhang <georgewujizhang@gmail.com>
2020-12-06 09:32:00 -0800

    Js Outline + gitignore
    Things that need to be done in js.

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//
+// Shortcut: Enable Menu Selection
+//
+
+//
+// Shortcut: Enable Menu Selection
+//
+
+//
+// Styling: Writing Tab Indent
+//
+
+//
+// Storage: Save Text Content In Editor
+//
+
+//write to local storage
+
+//read local storage
+
+//
+// Open Settings Menu
+//
+
+//
+// Close Settings
+//
+
+//
+// Settings Option
+//
+
+//change font size based on slider
+
+//turn off word counter
+
+//turn off memory calculator
+
+//turn off time
+
+//time counter
+
+//
+// Loop: for when you're too lazy to code and just want it to run every sec
+//
