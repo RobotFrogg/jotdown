7b63b21b11c271c6fb9976d89287f5ade315724f
Ratana <ratanawang8@gmail.com>
2020-12-06 11:42:32 -0800

    Add dark mode toggle

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ function init(){
         loadSettings()
 }
 
+// Toggle Colour Theme
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+btn.addEventListener("click", function () {
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+    } else {
+        document.body.classList.toggle("dark-theme");
+    }
+});
 
 // Textarea Auto-Resize //
 $(function() {
